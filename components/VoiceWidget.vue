618240d4db23ecefe7ee6144252cc5368c86b95a
Itsme-CJ <crisjohnsalazar101@gmail.com>
2025-07-27 19:21:39 +0800

    Added

---
+++ components/VoiceWidget.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+    <vue-speech lang="en-US" @onTranscriptionEnd="handleEnd" />
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    handleEnd({ transcription }) {
+      this.$emit('transcription', transcription)
+    }
+  }
+}
+</script>
---
+++ pages/index.vue
@@ -0,0 +1,23 @@
+<template>
+  <div>
+    <h1>Voice to Text Test</h1>
+    <VoiceWidget @transcription="onTranscribe" />
+    <p>Transcript: {{ transcript }}</p>
+  </div>
+</template>
+
+<script>
+import VoiceWidget from '@/components/VoiceWidget'
+
+export default {
+  components: { VoiceWidget },
+  data() {
+    return { transcript: '' }
+  },
+  methods: {
+    onTranscribe(text) {
+      this.transcript = text
+    }
+  }
+}
+</script>
